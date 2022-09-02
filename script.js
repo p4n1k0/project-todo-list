01eb0064970c691a5bcb9fe31fe590075f708af4
Tarick <gabre.tarick@gmail.com>
2022-09-02 14:11:54 -0300

    commit para publicacao mas testes falhando

--- script.js
+++ script.js
@@ -1,49 +1,33 @@
-//Elementos do HTML
-let botao = document.getElementById('criar-tarefa');
-let input = document.getElementById('texto-tarefa');
-let listaPai = document.getElementById('lista-tarefas');
+// elementos html
+const taskIpt = document.getElementById('texto-tarefa');
+const taskBtn = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
 
-//Adiciona tarefas a listaPai usando input e button
-function adicionaItem() {
-    botao.addEventListener('click', function (){
-        const criaLi = document.createElement('li');
-        listaPai.appendChild(criaLi);
-        criaLi.innerText = input.value;
-        input.value = ''; 
-    });
-} 
+function addTask() {
+  taskIpt.addEventListener('click', () => {
+    const createLi = document.createElement('li');
 
-adicionaItem();
-
-//Muda a cor de bg ao clicar em algum item da lista
-const listaFilha = document.getElementsByTagName('li'); // elemento html
-function mudaBgLi() {
-    listaPai.addEventListener('click', function (event) {
-        
-        for (let i = 0; i < listaFilha.length; i += 1) {
-            
-            if (listaFilha[i].className === 'gray') {
-                listaFilha[i].classList.remove('gray')
-            }
-            if (listaFilha[i].className !== 'gray') {
-                event.target.classList.add('gray');
-            }
-        }
-    });
+    taskList.appendChild(createLi);
+    createLi.innerText = taskBtn.value;
+    taskBtn.value = '';
+  });
 }
-mudaBgLi();
 
-//Riscando tarefa completada
-//ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
-function completaTarefa() {
-    listaPai.addEventListener('dblclick', function (event) {
-        const elemento = event.target;
+addTask();
 
-        if (elemento.className.includes('completed')) {
-            elemento.classList.remove('completed');
-        } else {
-            elemento.classList.add('completed');
-        }
-    });
-} 
-completaTarefa();   
+const getLi = document.getElementById('li'); // elemento html lista de tarefas criadas
+
+function changeBackground() {
+  getLi.addEventListener('click', (change) => {
+    for (let i = 0; i < getLi.length; i += 1) {
+      if (getLi[i].className === 'gray') {
+        getLi[i].classList.remove('gray');
+      }
+      if (getLi[i].className !== 'gray') {
+        change.target.classList.add('gray');
+      }
+    }
+  });
+}
+
+changeBackground();
