af1019955924d5428e8073c54b9b398c81c7e6a9
p4n1k0 <gabre.tarick@gmail.com>
2024-07-17 16:15:21 -0300

    att

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let taskBtn = document.getElementById('criar-tarefa');
 taskBtn.addEventListener('click', addTask);
 
 let rmTasksBtn = document.getElementById('apaga-tudo');
-let svBtn = document.getElementById('salvar-tarefas');
+const svBtn = document.getElementById('salvar-tarefas');
 let rmFinBtn = document.getElementById('remover-finalizados');
 const rmSelectedBtn = document.getElementById('remover-selecionado')
+const mvUpBtn = document.getElementById('mover-cima');
 
 
 rmTasksBtn.addEventListener('click', function () {
@@ -38,13 +39,34 @@ function lineTask(click) {
 // Fonte clickTask e lineTask Luis Castro trybe
 
 function addTask() {
-  let task = document.createElement('li');
+  const task = document.createElement('li');
   task.innerText = newTask.value;
+  task.className = 'tarefa'
   task.addEventListener('click', clickTask);
   task.addEventListener('dblclick', lineTask);
   taskList.appendChild(task);
   newTask.value = '';
 }
+
+taskList.addEventListener('click', (event) => {
+  const selectedTask = event.target;
+  if (event.target.className === 'tarefa') {
+    for (let i = 0; i < taskItems.length; i += 1) {
+      taskItems[i].style.removeProperty('background-color');
+    }
+    selectedTask.style.backgroundColor = 'gray';
+  }
+});
+
+taskList.addEventListener('dblclick', (e) => {
+  const selectedTask = e.target;
+
+  if (e.target.className === 'tarefa') {
+    selectedTask.className += 'completed';
+  } else if (e.target.className === 'tarefa completed') {
+    selectedTask.className = 'tarefa';
+  }
+});
 
 rmFinBtn.addEventListener('click', function () {
   let element = document.querySelectorAll('.completed');
@@ -59,19 +81,8 @@ rmSelectedBtn.addEventListener('click', () => {
   taskSelected.remove()
 });
 
-
-function createTagLi(task) {
-  let li = document.createElement('li');
-  let spam = document.createElement('span');
-  spam.classList.add('textoTarefa');
-  spam.innerHTML = task.name;
-  li.appendChild(spam);
-
-  return li;
-}
-
 function finalizads(e) {
-  let iCompleted = e.target;
+  const iCompleted = e.target;
   if (iCompleted.classList.contains('completed')) {
     e.target.classList.remove('completed');
   } else {
@@ -80,6 +91,22 @@ function finalizads(e) {
 }
 taskList.addEventListener('dbclick', finalizads);
 
-rmBtn.addEventListener('click', function () {
-  let finalizads = document.querySelectorAll('.completed');
-})
+function saveTask() {
+  const tasks = taskList.innerHTML;
+  localStorage.setItem('tasks', tasks)
+}
+
+function initRenderization() {
+  if (localStorage.getItem('tasks') === null) {
+    localStorage.setItem('tasks', '');
+  } else {
+    const taskStorage = localStorage.getItem('tasks');
+    taskList.innerHTML = taskStorage;
+  }
+}
+
+svBtn.addEventListener('click', saveTask);
+
+window.onload = () => {
+  initRenderization();
+}
